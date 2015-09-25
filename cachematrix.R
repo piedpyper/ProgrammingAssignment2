926de187922196a47ed8c06b77dbbcde46d64842
pyper <ksjob12@gmail.com>
2015-09-25 19:48:54 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,7 @@
-##creates a special “matrix” object that can cache its inverse.
+##Write a pair of functions that cache the inverse of a matrix.
+
+
+##Creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
   
   inv <- NULL
